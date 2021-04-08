f86110f1cf05f2eb68680b1cc6727ad3910a2e3c
LuisRhomo <76223952+LuisRhomo@users.noreply.github.com>
2021-04-07 19:58:29 -0500

    Add files via upload

---
+++ correo.php
@@ -0,0 +1,13 @@
+<?php
+    //En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
+    $destino = 'luiswischo@gmail.com';
+    $nombre = $_POST['nombre'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    $email = $_POST['email'];
+    $header = "Enviado desde la pagina de Inmoviliaria";
+    $contenido = $mensaje . "\nAtentamente " . $nombre;
+    mail($destino, $asunto, $contenido,$header);
+    echo "<script>alert('correo enviado exitosamente')</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";    
+?>
