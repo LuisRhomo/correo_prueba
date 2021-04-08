144e599ea2191bf5aedd3fc038e5a4099ee9659c
LuisRhomo <76223952+LuisRhomo@users.noreply.github.com>
2021-04-07 20:13:34 -0500

    Update correo.php

--- correo.php
+++ correo.php
@@ -1,12 +1,13 @@
 <?php
-    //En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
-    $destino = 'luiswischo@gmail.com';
-    $nombre = $_POST['nombre'];
-    $asunto = $_POST['asunto'];
-    $mensaje = $_POST['mensaje'];
-    $email = $_POST['email'];
-    $header = "Enviado desde la pagina de Inmoviliaria";
-    $contenido = $mensaje . "\nAtentamente " . $nombre;
-    mail($destino, $asunto, $contenido,$header);
-    echo "<script>alert('correo enviado exitosamente')</script>"; 
+        //En el destino colocar el correo alque quieres que lleguen los datos del contacto de tu formulario
+ $destino = "luiswischo@gmail.com";
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $email = $_POST["email"];
+    $numero = $_POST["numero"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre: " . $nombre . "\nApellido: " . $apellido . "\nCorreo: " . $email . "\nNúmero: " . $numero . "\nAsunto: " . $mensaje;
+    mail($destino, "Contacto", $contenido);
+    header("Location: A_Donde_Lo_Quieras_Redireccionar");
+//Esto es opcional, aqui pueden colocar un mensaje de "enviado correctamente" o redireccionarlo a algun lugar
 ?>
